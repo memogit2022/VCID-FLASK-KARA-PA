8f51a86e27c8d2ca232eb78168d835fa7b82751c
memogit2022 <106697443+memogit2022@users.noreply.github.com>
2024-09-17 12:05:59 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sichereswort'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kara:mehmet@localhost/prakaravcdi'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'PASSWORD'@'DB-Adresse'/'datenbank-name''
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
