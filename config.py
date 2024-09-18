ab2e25245189869c712dc13f95975069e6518061
memogit2022 <106697443+memogit2022@users.noreply.github.com>
2024-09-18 08:16:51 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sichereswort'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kara:mehmet@20.3.236.63/prakaravcdi'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kara:mehmet@20.3.236.63/prakaravcdi?ssl_disabled=True'
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:'PASSWORD'@'DB-Adresse'/'datenbank-name''
     SQLALCHEMY_TRACK_MODIFICATIONS = False
